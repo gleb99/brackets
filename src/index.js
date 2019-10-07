a761d91795142fe2ea6e1e2f5c163d71b8127169
Gleb <gllukutin@gmail.com>
2019-10-07 18:06:00 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strN = str;
+  if (str.length % 2 === 0) {
+    for (let i = 0; i <= str.length / 2; i++) {
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      if (str.match(/\d/)) {
+        let left = bracketsConfig[i][0];
+        let right = bracketsConfig[i][1];
+        let summ = left + right;
+       if (strN.match(new RegExp(summ))!== null) {
+       strN =  strN.replace(new RegExp(summ), '');
+      }
+    } else { 
+      let left = "\\" + bracketsConfig[i][0];
+      let right ="\\" + bracketsConfig[i][1];
+      let summ = left + right;
+      if (strN.match(new RegExp(summ))!== null) {
+      strN =  strN.replace(new RegExp(summ), '');
+        }
+       }
+      }
+    }
+    if (strN === '') {
+      return true;
+    } else {
+      return false;
+    }
+  } else { return false};
 }
